577a0b69fecabb2c895bc481944a81381dd9efb5
Michal Töpfer <michal.topfer@gmail.com>
2023-08-31 12:34:33 +0200

    Save posts to database

--- db_model.py
+++ db_model.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from flask_bcrypt import Bcrypt
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, backref
 
 from abstract import Abstract
 
@@ -54,3 +57,20 @@ class User(db.Model, AbstractUser):
 
     def set_password(self, password_plain):
         self.password = bcrypt.generate_password_hash(password_plain)
+
+
+class Post(db.Model):
+
+    __tablename__ = "posts"
+
+    id_post = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey(User.id_user, ondelete='RESTRICT'))
+    src = db.Column(db.String(256), nullable=False)
+    timestamp = db.Column(db.DateTime)
+
+    user = relationship("User", backref=backref("users", uselist=False))
+
+    def __init__(self, src, id_user):
+        self.id_user = id_user
+        self.src = src
+        self.timestamp = datetime.now()
--- upload.py
+++ upload.py
@@ -1,46 +1,54 @@
 import os
+import uuid
 
 from flask import redirect, request, flash, Blueprint, url_for, send_from_directory
-from flask_login import login_required
-from werkzeug.utils import secure_filename
+from flask_login import login_required, current_user
 
 from config import config
+from db_model import Post, db
 from helpers import render
 
 upload_blueprint = Blueprint('upload', __name__, template_folder='templates', static_folder='static')
 
-allowed_extensions = ", .".join(config['allowed_extensions'])
-
-
-def allowed_file(filename: str):
-    print(filename)
-    return '.' in filename and \
-       filename.rsplit('.', 1)[1].lower() in config['allowed_extensions']
+allowed_extensions = ", ".join(f".{ext}" for ext in config['allowed_extensions'])
 
 
 @upload_blueprint.route('/upload', methods=("GET", "POST"))
 @login_required
 def upload():
     if request.method == "POST":
-
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('Musíte vybrat soubor!', 'danger')
             return redirect(request.url)
         file = request.files['file']
+
         # If the user does not select a file, the browser submits an empty file without a filename.
-        if file.filename == '':
+        if not file or file.filename == '':
             flash('Musíte vybrat soubor!', 'danger')
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(config['upload_folder'], filename))
-            flash(f"Soubor byl úspěšně nahrán.", "success")
-            return redirect(url_for('upload.download_file', name=filename))
+        # check the extension
+        if '.' not in file.filename:
+            extension = ""
         else:
-            flash(f"Soubor není podporován, povolené přípony jsou: {allowed_extensions}.", "danger")
+            extension = file.filename.rsplit('.', 1)[1].lower()
+        if extension not in config['allowed_extensions']:
+            flash(f"Soubor s příponou '{extension}' není podporován, povolené přípony jsou: {allowed_extensions}.", "danger")
             return redirect(request.url)
+
+        # save the uploaded file
+        filename = f"{current_user.id_user}_{uuid.uuid4()}.{extension}"
+        file_path = os.path.join(config['upload_folder'], filename)
+        file.save(file_path)
+
+        # create a post in database
+        post = Post(file_path, current_user.id_user)
+        db.session.add(post)
+        db.session.commit()
+
+        flash(f"Soubor byl úspěšně nahrán.", "success")
+        return redirect("/")
 
     return render("upload.html", title="Nahrát soubor", allowed_extensions=allowed_extensions)
 
