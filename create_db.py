b3b87fbfd16f0a79424c438b940704bc2fa5c884
Michal Töpfer <michal.topfer@gmail.com>
2023-09-05 13:06:27 +0200

    Add default location when creating the database for the first time

--- create_db.py
+++ create_db.py
@@ -1,4 +1,10 @@
+from db_model import Location
 from tmmgram import db, app
 
 with app.app_context():
     db.create_all()
+
+    if db.session.query(Location).first() is None:  # no locations in the database
+        default_location = Location("Kdekoli", 1)
+        db.session.add(default_location)
+        db.session.commit()
