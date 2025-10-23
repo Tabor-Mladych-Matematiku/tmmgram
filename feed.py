fedb6cc10e16d88b0af3f44894062fc87b4deffb
David Šosvald <david.sosvald@email.cz>
2025-10-23 11:35:41 +0200

    Include date in feed

--- feed.py
+++ feed.py
@@ -3,6 +3,7 @@ from flask_login import login_required
 
 from db_model import Post
 from helpers import render
+from datetime import datetime
 
 feed_blueprint = Blueprint('feed', __name__, template_folder='templates', static_folder='static')
 
@@ -15,4 +16,7 @@ def render_feed():
                          .order_by(Post.timestamp.desc())
                          .limit(10)
                          .all())
+    # Format the timestamp in Czech format (DD.MM.YYYY HH:MM)
+    for post in posts:
+        post.time = post.timestamp.strftime('%d.%m.%Y %H:%M')
     return render('index.html', posts=posts)
